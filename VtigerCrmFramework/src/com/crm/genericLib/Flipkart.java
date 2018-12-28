744dd6d35aa692b8668447145c1821f58c6a9e46
Pankaj Bhatia <pankajbhatia.pb@gmail.com>
2018-12-28 23:52:33 +0530

    test

--- VtigerCrmFramework/src/com/crm/genericLib/Flipkart.java
+++ VtigerCrmFramework/src/com/crm/genericLib/Flipkart.java
@@ -1,5 +1,29 @@
 package com.crm.genericLib;
 
-public class Flipkart {
+ class Flipkart {
+	int count;
+	
+	public Flipkart() {
+		count++;
+	}
+	public int getCountOfObject()
+	{
+		return count;
+	}
+
+	public static class mainTest{
+		
+		public static void main(String[] args)
+		{
+			Flipkart f1=new Flipkart();
+			Flipkart f2=new Flipkart();
+			Flipkart f3=new Flipkart();
+			
+			//test
+		}
+		
+		
+		
+	}
 	
 }
